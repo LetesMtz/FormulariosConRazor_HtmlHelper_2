8148b0703e1223d4df211ab5688021e32956af47
Carlos Alejandro Murga Martínez <carlos.murga1@catolica.edu.sv>
2024-04-05 23:42:59 -0600

    CrearEquipos

---
+++ FormulariosConRazor_HtmlHelper_2/Models/equiposDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FormulariosConRazor_HtmlHelper_2.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<marcas> marcas { get; set; }
+    }
+}
--- FormulariosConRazor_HtmlHelper_2/Program.cs
+++ FormulariosConRazor_HtmlHelper_2/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using FormulariosConRazor_HtmlHelper_2.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,7 @@ builder.Services.AddControllersWithViews();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<usuariosDbContext>(opt =>
+builder.Services.AddDbContext<equiposDbContext>(opt =>
         opt.UseSqlServer(
             builder.Configuration.GetConnectionString("equiposDbConnection")
             )
