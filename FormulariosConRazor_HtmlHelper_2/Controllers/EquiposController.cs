e5aecb5f9fb1f92d0079744e4fa99d24a4506cdd
Carlos Alejandro Murga Martínez <carlos.murga1@catolica.edu.sv>
2024-04-05 22:38:13 -0600

    Agregar archivos de proyecto.

---
+++ FormulariosConRazor_HtmlHelper_2/Controllers/EquiposController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormulariosConRazor_HtmlHelper_2.Controllers
+{
+    public class EquiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FormulariosConRazor_HtmlHelper_2/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<usuariosDbContext>(opt =>
+        opt.UseSqlServer(
+            builder.Configuration.GetConnectionString("equiposDbConnection")
+            )
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
